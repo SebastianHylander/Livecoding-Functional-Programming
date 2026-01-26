111ca9814a94d0eb6a43eaaec220d05ac79d97b9
Sebastian Cloos Hylander <sehy@itu.dk>
2026-01-26 11:45:54 +0100

    guessanumber

---
+++ session_01/guessanumber/Program.fs
@@ -0,0 +1,24 @@
+﻿
+open System
+
+let sc = Kattio.Scanner()
+let n = new Random () |> fun r -> 1 + r.Next (100) 
+
+let rec guess () = 
+    let i = sc.NextInt()
+    if i = n then
+        printfn "Correct the number was %d" n
+        1
+    else 
+        if i < n then
+            printfn "Your guess is too low"
+        else 
+            printfn "Your guess is too high"
+        
+        guess() + 1 
+    
+
+printfn "Welcome to the Guessing game!"
+printfn "I have a number between 1 and 100"
+printfn "Get guessing!"
+printfn "You used %d guesses" (guess ())
