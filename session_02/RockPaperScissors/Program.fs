86307d3e684c4a791a64ae066da437f8831d7882
Emilia-Victoria <emiliav.helsted@gmail.com>
2026-02-03 08:23:31 +0100

    Add rockpaperscissors

--- session_02/RockPaperScissors/Program.fs
+++ session_02/RockPaperScissors/Program.fs
@@ -1,2 +1,57 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+type Move =
+    | Rock
+    | Paper
+    | Scissors
+
+type Result =
+    | Win
+    | Loss
+    | Draw    
+
+// Determines result for player1
+let game player1 player2 = 
+    match player1,player2 with
+    | Rock, Scissors -> Win
+    | Rock, Paper -> Loss
+    | Paper, Rock -> Win
+    | Paper, Scissors -> Loss
+    | Scissors, Paper -> Win
+    | Scissors, Rock -> Loss
+    | _,_ -> Draw
+
+// Generates a random move
+let randomMove() =
+    let n = new Random () |> fun r -> r.Next(3)
+    match n with
+        | 0 -> Rock
+        | 1 -> Paper
+        | 2 -> Scissors
+
+let stringToMove(s) = 
+    match s with
+    | "rock" -> Rock
+    | "paper" -> Paper
+    | "scissors" -> Scissors
+    | _ -> exit 1
+
+let printResult(r) =
+    match r with
+    | Win -> printfn "You won!"
+    | Loss -> printfn "You lose. Better luck next time!"
+    | Draw -> printfn "It's a draw"
+    
+[<EntryPoint>]
+let main argv =
+    let sc = Kattio.Scanner()
+    
+    printfn "Let's play Rock, Paper, Scissors!"
+    printfn "Enter your move (rock/paper/scissors): "
+    let playerMove = sc.Next() |> stringToMove
+    let rndMove = randomMove()
+    let result = game playerMove rndMove
+
+    printfn "Opponent played %A"  rndMove
+    printResult(result)
+    0
