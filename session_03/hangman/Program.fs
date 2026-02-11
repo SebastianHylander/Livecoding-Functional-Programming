ef6c292a94710c99c0f27687693fb7f2cc6d0425
Slug-Boi <theis.p.holm@gmail.com>
2026-02-11 11:12:09 +0100

    add better print and add lives

--- session_03/hangman/Program.fs
+++ session_03/hangman/Program.fs
@@ -15,6 +15,9 @@ let prettyPrint (set: char list) =
     List.map (fun ch -> printf "%c " ch) set |> ignore
     printfn ""
 
+let GoodPrettyPrint (set: char list) =
+    System.String.Join(" ", Array.ofList(set))
+
 let check guessed =
     List.map
         (fun (ch: char) ->
@@ -28,18 +31,21 @@ let validate guessed =
 
 let sc = Kattio.Scanner()
 
-let rec run (guessed: char Set) =
+let rec run lives (guessed: char Set) =
+    if lives = 0 then "You died lol" else
+
     let new_guessed = sc.NextChar() |> System.Char.ToUpper |> guessed.Add
     check new_guessed |> prettyPrint
+
 
     match validate new_guessed with
     | true -> "Congratulations you won"
     | _ ->
         printfn "You current guesses are:"
         prettyPrint (Set.toList new_guessed)
-        run new_guessed
+        run (lives - 1) new_guessed
 
 printfn "Welcome to Hangman where no men will be hanged (PG 7)"
 check Set.empty |> prettyPrint
 
-printfn "%s" (run Set.empty)
+printfn "%s" (run 10000 Set.empty)
